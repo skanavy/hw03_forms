dbd491f332a498a3f3d961794670dd78adfb732b
skanavy <99424759+skanavy@users.noreply.github.com>
2022-08-20 10:02:56 +0300

    final14

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -6,9 +6,11 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('group/<slug:slug>/', views.group_posts, name='groups'),
-    path('profile/<str:username>/', views.profile, name='profile'),
-    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
     path('create/', views.post_create, name='post_create'),
+    path('group/<slug:slug>/', views.group_posts, name='groups'),
     path('posts/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('profile/<str:username>/', views.profile, name='profile'),
+
+
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,48 +5,49 @@ from django.shortcuts import get_object_or_404, redirect, render
 from .forms import PostForm
 from .models import Group, Post, User
 
+MAX_POSTS = 10
 
-def pgn(post_list, request):
-    paginator = Paginator(post_list, 10)
+
+def paginate_queryset(post_list, request):
+    paginator = Paginator(post_list, MAX_POSTS)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return page_obj
 
 
 def index(request):
-    post_list = Post.objects.select_related('author').all()
+    post_list = Post.objects.select_related('author', 'group')
     context = {
-        'page_obj': pgn(post_list, request),
+        'page_obj': paginate_queryset(post_list, request),
     }
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    post_list = group.group.all()
+    post_list = group.group.select_related('author', 'group')
     context = {
         'group': group,
-        'page_obj': pgn(post_list, request),
+        'page_obj': paginate_queryset(post_list, request),
     }
     return render(request, 'posts/group_list.html', context)
 
 
 def profile(request, username):
     user = User.objects.get(username=username)
-    post_list = Post.objects.filter(author=user)
-    post_count = Post.objects.filter(author=user).count()
+    post_list = user.posts.select_related('author')
+    post_count = post_list.count()
     context = {
         'post_count': post_count,
         'author': user,
-        'page_obj': pgn(post_list, request),
+        'page_obj': paginate_queryset(post_list, request),
     }
     return render(request, 'posts/profile.html', context)
 
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
-    user = post.author
-    post_count = Post.objects.filter(author=user).count()
+    post_count = Post.objects.filter(author=post.author).count()
     context = {
         'post': post,
         'post_count': post_count,
@@ -68,7 +69,7 @@ def post_create(request):
 @login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    form = PostForm(request.POST, instance=post)
+    form = PostForm(request.POST or None, instance=post)
     if request.user != post.author:
         return redirect('posts:post_detail', post_id=pk)
     if form.is_valid():
